1874b3b871a4d913477f82f9bfaa8a1d5b380ae4
augustine <ashramwen@gmail.com>
2016-10-18 15:14:32 +0800

    location tag change

--- client/src/app/components/Portal/OfficeSpace/OfficeSpace.controller.js
+++ client/src/app/components/Portal/OfficeSpace/OfficeSpace.controller.js
@@ -5,19 +5,19 @@ angular.module('BigScreen.Portal')
 .controller('OfficeSpaceController', ['$scope', function($scope) {
 
     $scope.firstRooms = [
-        { location: '0807w-W05', name: '工位区', type: 'work' },
-        { location: '0807w-W04', name: '工位区', type: 'work' },
-        { location: '0807w-F03', name: '休闲区', type: 'rest' },
-        { location: '0807w-W03', name: '工位区', type: 'work' },
-        { location: '0807w-W02', name: '工位区', type: 'work' },
-        { location: '0807w-W01', name: '工位区', type: 'work' },
-        { location: '0807w-F02', name: '休闲区', type: 'rest' },
-        { location: '0807w-F01', name: '休闲区', type: 'rest' }
+        { location: '0807w-E', name: '工位区', type: 'work' },
+        { location: '0807w-D', name: '工位区', type: 'work' },
+        { location: '0807w-Y03', name: '休闲区', type: 'rest' },
+        { location: '0807w-C', name: '工位区', type: 'work' },
+        { location: '0807w-B', name: '工位区', type: 'work' },
+        { location: '0807w-A', name: '工位区', type: 'work' },
+        { location: '0807w-Y02', name: '休闲区', type: 'rest' },
+        { location: '0807w-Y01', name: '休闲区', type: 'rest' }
     ];
 
     $scope.secondRooms = [
-        { location: '0807w-F06', name: '休闲区', type: 'rest' },
-        { location: '0807w-F05', name: '休闲区', type: 'rest' },
-        { location: '0807w-F04', name: '休闲区', type: 'rest' }
+        { location: '0807w-Y06', name: '休闲区', type: 'rest' },
+        { location: '0807w-Y05', name: '休闲区', type: 'rest' },
+        { location: '0807w-Y04', name: '休闲区', type: 'rest' }
     ];
 }]);
--- client/src/app/components/Portal/OfficeUsage/OfficeUsage.controller.js
+++ client/src/app/components/Portal/OfficeUsage/OfficeUsage.controller.js
@@ -13,42 +13,42 @@ angular.module('BigScreen.Portal')
         sign: 'eba8612347af80999a60e9d32ff9a270',
         no: '8-7-11-N',
         name: '十里亭',
-        location: '0807W-M07'
+        location: '0807w-W07'
     }, {
         id: 25,
         sign: 'af742effbfbabb0c0043e113707a7b99',
         no: '8-7-13-N',
         name: '幽篁里',
-        location: '0807W-M06'
+        location: '0807w-W06'
     }, {
         id: 26,
         sign: 'f12bd9d4fb58a2be67d821d16a04248d',
         no: '8-7-12-N',
         name: '文昌阁',
-        location: '0807W-M05'
+        location: '0807w-W05'
     }, {
         id: 27,
         sign: '43ae0b1b9638aed3e693b1990a1484ff',
         no: '8-7-15-N',
         name: '勿幕阁',
-        location: '0807W-M03'
+        location: '0807w-W03'
     }, {
         id: 28,
         sign: 'bb8d610ad4116a57cd483dddfa8b589c',
         no: '8-7-14-N',
         name: '安定阁',
-        location: '0807W-M04'
+        location: '0807w-W04'
     }, {
         id: 29,
         sign: '3f9d862dbacb88651348c35d74be0419',
         no: '8-7-16-N',
         name: '安远阁',
-        location: '0807W-M01'
+        location: '0807w-W01'
     }, {
         id: 30,
         sign: 'c262abdabff07f170a5b37afdfa272b8',
         no: '8-7-17-N',
         name: '尚检阁',
-        location: '0807W-M02'
+        location: '0807w-W02'
     }];
 }]);
