be739cc0268a1ef5ad9cbc18b39c2a3214d33d8c
augustine <ashramwen@gmail.com>
2016-12-14 16:35:29 +0800

    update some text

--- client/src/app/components/Portal/OfficeUsage/OfficeUsage.controller.js
+++ client/src/app/components/Portal/OfficeUsage/OfficeUsage.controller.js
@@ -11,43 +11,43 @@ angular.module('BigScreen.Portal')
     $scope.rooms = [{
         id: 24,
         sign: 'eba8612347af80999a60e9d32ff9a270',
-        no: '8-7-11-N',
+        no: '8-7-1-W',
         name: '十里亭',
         location: '0807w-W07'
     }, {
         id: 25,
         sign: 'af742effbfbabb0c0043e113707a7b99',
-        no: '8-7-13-N',
+        no: '8-7-2-W',
         name: '幽篁里',
         location: '0807w-W06'
     }, {
         id: 26,
         sign: 'f12bd9d4fb58a2be67d821d16a04248d',
-        no: '8-7-12-N',
+        no: '8-7-3-W',
         name: '文昌阁',
         location: '0807w-W05'
     }, {
         id: 27,
         sign: '43ae0b1b9638aed3e693b1990a1484ff',
-        no: '8-7-15-N',
+        no: '8-7-4-W',
         name: '勿幕阁',
         location: '0807w-W03'
     }, {
         id: 28,
         sign: 'bb8d610ad4116a57cd483dddfa8b589c',
-        no: '8-7-14-N',
+        no: '8-7-5-W',
         name: '安定阁',
         location: '0807w-W04'
     }, {
         id: 29,
         sign: '3f9d862dbacb88651348c35d74be0419',
-        no: '8-7-16-N',
+        no: '8-7-8-W',
         name: '安远阁',
         location: '0807w-W01'
     }, {
         id: 30,
         sign: 'c262abdabff07f170a5b37afdfa272b8',
-        no: '8-7-17-N',
+        no: '8-7-9-W',
         name: '尚检阁',
         location: '0807w-W02'
     }];
