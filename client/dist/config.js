b910ee51e45dd33701df3eefa03cbcd5751bbc75
augustine <ashramwen@gmail.com>
2017-03-02 18:25:29 +0800

    #710

--- client/dist/config.js
+++ client/dist/config.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     window.appConfig = {
         'DEV': {
             'cloudUrl': 'http://api-development-beehivecn3.internal.kii.com',
@@ -9,6 +9,14 @@
             'thirdPartyAPIKey': '138ef89effc5be05830170266763dbba8ac0be0f'
         },
         'QA': {
+            'cloudUrl': 'http://api-development-beehivecn3.internal.kii.com',
+            'wsUrl': 'ws://114.215.178.24:8080/beehive-portal/websocket/stomp',
+            'siteUrl': 'http://114.215.178.24:8080/beehive-portal',
+            'thirdPartyAPIUrl': 'http://api.openibplatform.com/beehive/',
+            'thirdPartyWsUrl': 'ws://114.215.178.24:8081/3rdpartyapiserver/websocket/stomp',
+            'thirdPartyAPIKey': '138ef89effc5be05830170266763dbba8ac0be0f'
+        },
+        'PROD': {
             'cloudUrl': 'http://api-development-beehivecn3.internal.kii.com',
             'wsUrl': 'ws://114.215.178.24:8080/beehive-portal/websocket/stomp',
             'siteUrl': 'http://114.215.178.24:8080/beehive-portal',
